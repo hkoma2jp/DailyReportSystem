eba9adbd7e38808638869baa0c266923c608bf8a
hkoma2jp <h.koma2.jp@gmail.com>
2023-05-04 23:30:43 +0900

    Chapter 6.2 機能の作成 1.一覧表示
    ・/employee/list で一覧表示する

---
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.techacademy.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.techacademy.entity.Employee;
+import com.techacademy.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository repository) {
+        this.employeeRepository = repository;
+    }
+
+    /** 全件検索結果を返す */
+    public List<Employee> getEmployeeList() {
+        return employeeRepository.findAll();
+    }
+}
