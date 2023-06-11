10db9dcbe213482afbb780ce304a0cb739a26e21
hkoma2jp <h.koma2.jp@gmail.com>
2023-06-11 13:02:17 +0900

    Customize 外観の調整
    ・共通化したheader/footerをさらにログイン状態の反映
    ・各ページテンプレートデザインなどの微調整

--- src/main/java/com/techacademy/controller/LoginController.java
+++ src/main/java/com/techacademy/controller/LoginController.java
@@ -1,12 +1,22 @@
 package com.techacademy.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    /** ログイン時にログイン状態をテンプレートに渡したい */
     @GetMapping("/login")
-    public String getLogin() {
+    public String getLogin(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("isLoggedIn", true);
+            model.addAttribute("username", principal.getName());
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
         return "login";
     }
 
